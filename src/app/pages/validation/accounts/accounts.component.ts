c3e0184b9fdd4dcfd27958e90029fbcb820de520
eudycolmenares <eudy.developer@gmail.com>
2021-01-18 12:47:02 -0500

    Modulo Validacion Cuentas: ajustes

--- src/app/pages/validation/accounts/accounts.component.ts
+++ src/app/pages/validation/accounts/accounts.component.ts
@@ -7,6 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AccountsComponent implements OnInit {
+  // table
+  dataToTable: object[];
+  structure: object[] = [
+    {
+      name: 'code',
+      description: 'MONTO',
+      validation: '',
+    },
+    {
+      name: 'description',
+      description: 'CHARGE_SERVICE',
+      validation: '',
+    },
+    {
+      name: 'state',
+      description: 'CUSTNUM',
+      validation: 'active-desactive'
+    },
+    {
+      name: 'compensate',
+      description: 'CUSTOMER_ID',
+      validation: 'yes-no',
+    },
+    {
+      name: 'maintenance',
+      description: 'CO_CODE',
+      validation: 'yes-no'
+    },
+    {
+      name: 'telephone',
+      description: 'CO_ID',
+      validation: 'service'
+    },
+    {
+      name: 'television',
+      description: 'VALID_FROM',
+      validation: 'service'
+    },
+    {
+      name: 'internet',
+      description: 'AMOUNT',
+      validation: 'service'
+    },
+    {
+      name: 'internet',
+      description: 'REMARK',
+      validation: 'service'
+    },
+  ];
 
   constructor() { }
 
