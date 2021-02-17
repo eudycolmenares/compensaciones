4c27de00010d6e09497915c677b18722f9d79a40
eudycolmenares <eudy.developer@gmail.com>
2021-02-17 16:57:52 -0500

    Menu: ajustes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -18,133 +18,266 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
     this.items = [
       {
-        label: 'Parametrización',
-        icon:'pi pi-fw pi-file',
-        // expanded: true,
+        label: 'RR',
         items: [
           {
-            label: 'Ajustes',
-            icon:'pi pi-fw pi-trash',
-            routerLink: ['/dashboard/settings']
+            label: 'Parametrización',
+            items: [
+              {
+                label: 'Cargar Fallas',
+                icon:'pi pi-fw pi-cloud-upload',
+                routerLink: ['/dashboard/load-faults']
+              },
+              {
+                label: 'Causas Ordenes Mantenimiento',
+                icon:'pi pi-fw pi-exclamation-circle',
+                routerLink: ['/dashboard/maintenance-orders-causes']
+              },
+              {
+                label: 'Síntomas Ordenes Mantenimiento',
+                icon:'pi pi-fw pi-search',
+                routerLink: ['/dashboard/orders-symptoms']
+              },
+              {
+                label: 'Validación Fallas RR',
+                icon:'pi pi-fw pi-check-square',
+                routerLink: ['/dashboard/rr-failure-validation']
+              }
+            ]
           },
           {
-            label: 'Estrato',
-            icon:'pi pi-fw pi-external-link',
-            routerLink: ['/dashboard/stratum']
+            label: 'Rentas',
+            items: [
+              {
+                label: 'Descargue Nodos-Cuentas',
+                icon:'pi pi-fw pi-download',
+                routerLink: ['/dashboard/rents-download']
+              },
+              {
+                label: 'Cargue Rentas RR',
+                icon:'pi pi-fw pi-upload',
+                routerLink: ['/dashboard/rents-load']
+              }
+            ]
           },
           {
-            label: 'Causas',
-            icon:'pi pi-fw pi-external-link',
-            routerLink: ['/dashboard/causes']
+            label: 'Facturación',
+            items: [
+              {
+                label: 'Cuentas Compensadas RR',
+                icon:'pi pi-fw pi-dollar',
+                routerLink: ['/dashboard/item-rr-billing']
+              }
+            ]
           },
-          {
-            label: 'Estado Sistema',
-            icon:'pi pi-fw pi-external-link',
-            routerLink: ['/dashboard/system-status']
-          },
-          {
-            label: 'Síntoma',
-            icon:'pi pi-fw pi-external-link',
-            routerLink: ['/dashboard/symptom']
-          },
-          {
-            label: 'Prioridades',
-            icon:'pi pi-fw pi-external-link',
-            routerLink: ['/dashboard/priorities']
-          },
-          {
-            label: 'Tipo Origen',
-            icon:'pi pi-fw pi-external-link',
-            routerLink: ['/dashboard/origintype']
-          },
-          {
-            label: 'Cargue Masivo',
-            icon:'pi pi-fw pi-external-link',
-            routerLink: ['/dashboard/bulk-load']
-          }
         ]
       },
       {
-        label: 'Parametrización RR',
-        icon:'pi pi-fw pi-pencil',
+        label: 'Inspira',
         items: [
           {
-            label: 'Cargar Fallas',
-            icon:'pi pi-fw pi-align-left',
-            routerLink: ['/dashboard/load-faults']
+            label: 'Parametrización',
+            items: [
+              {
+                label: 'Ajustes',
+                icon:'pi pi-fw pi-cog',
+                routerLink: ['/dashboard/settings']
+              },
+              {
+                label: 'Estrato',
+                icon:'pi pi-fw pi-sitemap',
+                routerLink: ['/dashboard/stratum']
+              },
+              {
+                label: 'Causas',
+                icon:'pi pi-fw pi-exclamation-circle',
+                routerLink: ['/dashboard/causes']
+              },
+              {
+                label: 'Estado Sistema',
+                icon:'pi pi-fw pi-shield',
+                routerLink: ['/dashboard/system-status']
+              },
+              {
+                label: 'Síntoma',
+                icon:'pi pi-fw pi-search',
+                routerLink: ['/dashboard/symptom']
+              },
+              {
+                label: 'Prioridades',
+                icon:'pi pi-fw pi-clock',
+                routerLink: ['/dashboard/priorities']
+              },
+              {
+                label: 'Tipo Origen',
+                icon:'pi pi-fw pi-link',
+                routerLink: ['/dashboard/origintype']
+              },
+              {
+                label: 'Cargue Masivo',
+                icon:'pi pi-fw pi-file-excel',
+                routerLink: ['/dashboard/bulk-load']
+              }
+            ]
           },
           {
-            label: 'Causas Ordenes Mantenimiento',
-            icon:'pi pi-fw pi-align-left',
-            routerLink: ['/dashboard/maintenance-orders-causes']
+            label: 'Validación',
+            items: [
+              {
+                label: 'Nodos',
+                icon:'pi pi-fw pi-circle-off',
+                routerLink: ['/dashboard/nodes-validation']
+              },
+              {
+                label: 'Cuentas',
+                icon:'pi pi-fw pi-book',
+                routerLink: ['/dashboard/validation-accounts']
+              }
+            ]
           },
           {
-            label: 'Síntomas Ordenes Mantenimiento',
-            icon:'pi pi-fw pi-align-left',
-            routerLink: ['/dashboard/orders-symptoms']
-          },
-          {
-            label: 'Validación Fallas RR',
-            icon:'pi pi-fw pi-align-left',
-            routerLink: ['/dashboard/rr-failure-validation']
-          }
-        ]
-      },
-      {
-        label: 'Validación',
-        icon:'pi pi-fw pi-pencil',
-        items: [
-          {
-            label: 'Nodos',
-            icon:'pi pi-fw pi-align-left',
-            routerLink: ['/dashboard/nodes-validation']
-          },
-          {
-            label: 'Cuentas',
-            icon:'pi pi-fw pi-align-left',
-            routerLink: ['/dashboard/validation-accounts']
-          }
-        ]
-      },
-      {
-        label: 'Rentas RR',
-        icon:'pi pi-fw pi-pencil',
-        items: [
-          {
-            label: 'Descargue Nodos-Cuentas',
-            icon:'pi pi-fw pi-align-left',
-            routerLink: ['/dashboard/rents-download']
-          },
-          {
-            label: 'Cargue Rentas RR',
-            icon:'pi pi-fw pi-align-left',
-            routerLink: ['/dashboard/rents-load']
-          }
-        ]
-      },
-      {
-        label: 'Facturación RR',
-        icon:'pi pi-fw pi-pencil',
-        items: [
-          {
-            label: 'Cuentas Compensadas RR',
-            icon:'pi pi-fw pi-align-left',
-            routerLink: ['/dashboard/item-rr-billing']
-          }
-        ]
-      },
-      {
-        label: 'Supervisión',
-        icon:'pi pi-fw pi-pencil',
-        items: [
-          {
-            label: 'Proceso RR',
-            icon:'pi pi-fw pi-align-left',
-            routerLink: ['/dashboard/process-rr']
+            label: 'Supervisión',
+            items: [
+              {
+                label: 'Proceso RR',
+                icon:'pi pi-fw pi-chart-line',
+                routerLink: ['/dashboard/process-rr']
+              }
+            ]
           }
         ]
       }
     ]
+
+    // this.items = [
+    //   {
+    //     label: 'Parametrización',
+    //     icon:'pi pi-fw pi-file',
+    //     // expanded: true,
+    //     items: [
+    //       {
+    //         label: 'Ajustes',
+    //         icon:'pi pi-fw pi-trash',
+    //         routerLink: ['/dashboard/settings']
+    //       },
+    //       {
+    //         label: 'Estrato',
+    //         icon:'pi pi-fw pi-external-link',
+    //         routerLink: ['/dashboard/stratum']
+    //       },
+    //       {
+    //         label: 'Causas',
+    //         icon:'pi pi-fw pi-external-link',
+    //         routerLink: ['/dashboard/causes']
+    //       },
+    //       {
+    //         label: 'Estado Sistema',
+    //         icon:'pi pi-fw pi-external-link',
+    //         routerLink: ['/dashboard/system-status']
+    //       },
+    //       {
+    //         label: 'Síntoma',
+    //         icon:'pi pi-fw pi-external-link',
+    //         routerLink: ['/dashboard/symptom']
+    //       },
+    //       {
+    //         label: 'Prioridades',
+    //         icon:'pi pi-fw pi-external-link',
+    //         routerLink: ['/dashboard/priorities']
+    //       },
+    //       {
+    //         label: 'Tipo Origen',
+    //         icon:'pi pi-fw pi-external-link',
+    //         routerLink: ['/dashboard/origintype']
+    //       },
+    //       {
+    //         label: 'Cargue Masivo',
+    //         icon:'pi pi-fw pi-external-link',
+    //         routerLink: ['/dashboard/bulk-load']
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     label: 'Parametrización RR',
+    //     icon:'pi pi-fw pi-pencil',
+    //     items: [
+    //       {
+    //         label: 'Cargar Fallas',
+    //         icon:'pi pi-fw pi-align-left',
+    //         routerLink: ['/dashboard/load-faults']
+    //       },
+    //       {
+    //         label: 'Causas Ordenes Mantenimiento',
+    //         icon:'pi pi-fw pi-align-left',
+    //         routerLink: ['/dashboard/maintenance-orders-causes']
+    //       },
+    //       {
+    //         label: 'Síntomas Ordenes Mantenimiento',
+    //         icon:'pi pi-fw pi-align-left',
+    //         routerLink: ['/dashboard/orders-symptoms']
+    //       },
+    //       {
+    //         label: 'Validación Fallas RR',
+    //         icon:'pi pi-fw pi-align-left',
+    //         routerLink: ['/dashboard/rr-failure-validation']
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     label: 'Validación',
+    //     icon:'pi pi-fw pi-pencil',
+    //     items: [
+    //       {
+    //         label: 'Nodos',
+    //         icon:'pi pi-fw pi-align-left',
+    //         routerLink: ['/dashboard/nodes-validation']
+    //       },
+    //       {
+    //         label: 'Cuentas',
+    //         icon:'pi pi-fw pi-align-left',
+    //         routerLink: ['/dashboard/validation-accounts']
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     label: 'Rentas RR',
+    //     icon:'pi pi-fw pi-pencil',
+    //     items: [
+    //       {
+    //         label: 'Descargue Nodos-Cuentas',
+    //         icon:'pi pi-fw pi-align-left',
+    //         routerLink: ['/dashboard/rents-download']
+    //       },
+    //       {
+    //         label: 'Cargue Rentas RR',
+    //         icon:'pi pi-fw pi-align-left',
+    //         routerLink: ['/dashboard/rents-load']
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     label: 'Facturación RR',
+    //     icon:'pi pi-fw pi-pencil',
+    //     items: [
+    //       {
+    //         label: 'Cuentas Compensadas RR',
+    //         icon:'pi pi-fw pi-align-left',
+    //         routerLink: ['/dashboard/item-rr-billing']
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     label: 'Supervisión',
+    //     icon:'pi pi-fw pi-pencil',
+    //     items: [
+    //       {
+    //         label: 'Proceso RR',
+    //         icon:'pi pi-fw pi-align-left',
+    //         routerLink: ['/dashboard/process-rr']
+    //       }
+    //     ]
+    //   }
+    // ]
   }
 
   toggleSidebar() {
