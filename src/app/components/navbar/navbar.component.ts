6826e2e3b4af76204436516727cfde9c977d7472
eudycolmenares <eudy.developer@gmail.com>
2021-02-18 16:42:07 -0500

    Componente Menu: ajustes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { MenuItem } from 'primeng/api';
+import { validateLocaleAndSetLanguage } from 'typescript';
 
 @Component({
   selector: 'app-navbar',
@@ -19,27 +20,33 @@ export class NavbarComponent implements OnInit {
     this.items = [
       {
         label: 'RR',
+        title: 'RR',
         items: [
           {
             label: 'Parametrización',
+            title: 'Parametrización',
             items: [
               {
                 label: 'Cargar Fallas',
+                title: 'Cargar Fallas',
                 icon:'pi pi-fw pi-cloud-upload',
                 routerLink: ['/dashboard/load-faults']
               },
               {
                 label: 'Causas Ordenes Mantenimiento',
+                title: 'Causas Ordenes Mantenimiento',
                 icon:'pi pi-fw pi-exclamation-circle',
                 routerLink: ['/dashboard/maintenance-orders-causes']
               },
               {
                 label: 'Síntomas Ordenes Mantenimiento',
+                title: 'Síntomas Ordenes Mantenimiento',
                 icon:'pi pi-fw pi-search',
                 routerLink: ['/dashboard/orders-symptoms']
               },
               {
                 label: 'Validación Fallas RR',
+                title: 'Validación Fallas RR',
                 icon:'pi pi-fw pi-check-square',
                 routerLink: ['/dashboard/rr-failure-validation']
               }
@@ -47,14 +54,17 @@ export class NavbarComponent implements OnInit {
           },
           {
             label: 'Rentas',
+            title: 'Rentas',
             items: [
               {
                 label: 'Descargue Nodos-Cuentas',
+                title: 'Descargue Nodos-Cuentas',
                 icon:'pi pi-fw pi-download',
                 routerLink: ['/dashboard/rents-download']
               },
               {
                 label: 'Cargue Rentas RR',
+                title: 'Cargue Rentas RR',
                 icon:'pi pi-fw pi-upload',
                 routerLink: ['/dashboard/rents-load']
               }
@@ -62,9 +72,11 @@ export class NavbarComponent implements OnInit {
           },
           {
             label: 'Facturación',
+            title: 'Facturación',
             items: [
               {
                 label: 'Cuentas Compensadas RR',
+                title: 'Cuentas Compensadas RR',
                 icon:'pi pi-fw pi-dollar',
                 routerLink: ['/dashboard/item-rr-billing']
               }
@@ -74,47 +86,57 @@ export class NavbarComponent implements OnInit {
       },
       {
         label: 'Inspira',
+        title: 'Inspira',
         items: [
           {
             label: 'Parametrización',
+            title: 'Parametrización',
             items: [
               {
                 label: 'Ajustes',
+                title: 'Ajustes',
                 icon:'pi pi-fw pi-cog',
                 routerLink: ['/dashboard/settings']
               },
               {
                 label: 'Estrato',
+                title: 'Estrato',
                 icon:'pi pi-fw pi-sitemap',
                 routerLink: ['/dashboard/stratum']
               },
               {
                 label: 'Causas',
+                title: 'Causas',
                 icon:'pi pi-fw pi-exclamation-circle',
                 routerLink: ['/dashboard/causes']
               },
               {
                 label: 'Estado Sistema',
+                title: 'Estado Sistema',
                 icon:'pi pi-fw pi-shield',
                 routerLink: ['/dashboard/system-status']
               },
               {
                 label: 'Síntoma',
+                title: 'Síntoma',
                 icon:'pi pi-fw pi-search',
                 routerLink: ['/dashboard/symptom']
               },
               {
                 label: 'Prioridades',
+                title: 'Prioridades',
                 icon:'pi pi-fw pi-clock',
                 routerLink: ['/dashboard/priorities']
               },
               {
                 label: 'Tipo Origen',
+                title: 'Tipo Origen',
                 icon:'pi pi-fw pi-link',
                 routerLink: ['/dashboard/origintype']
               },
               {
                 label: 'Cargue Masivo',
+                title: 'Cargue Masivo',
                 icon:'pi pi-fw pi-file-excel',
                 routerLink: ['/dashboard/bulk-load']
               }
@@ -122,14 +144,17 @@ export class NavbarComponent implements OnInit {
           },
           {
             label: 'Validación',
+            title: 'Validación',
             items: [
               {
                 label: 'Nodos',
+                title: 'Nodos',
                 icon:'pi pi-fw pi-circle-off',
                 routerLink: ['/dashboard/nodes-validation']
               },
               {
                 label: 'Cuentas',
+                title: 'Cuentas',
                 icon:'pi pi-fw pi-book',
                 routerLink: ['/dashboard/validation-accounts']
               }
@@ -137,9 +162,11 @@ export class NavbarComponent implements OnInit {
           },
           {
             label: 'Supervisión',
+            title: 'Supervisión',
             items: [
               {
                 label: 'Proceso RR',
+                title: 'Proceso RR',
                 icon:'pi pi-fw pi-chart-line',
                 routerLink: ['/dashboard/process-rr']
               }
