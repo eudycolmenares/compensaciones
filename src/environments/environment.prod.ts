185bb209b5eab3af7a64cdbc0ea6510423f94968
eudycolmenares <eudy.developer@gmail.com>
2021-04-06 22:54:14 -0500

    Enviroment produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -18,6 +18,7 @@ export const environment = {
   urlApi_rules: domain_compensaciones + '/WSCompensaciones-web/webresources/WSNodesRules/',
   urlApi_Businessrules: domain_compensaciones + '/WSCompensacionesBatchRR-web/webresources/WSBusinessRulesRR/',
   urlApi_BillingFiles: domain_compensaciones + '/WSCompensacionesBatchRR-web/webresources/WSBilligRR/',
+  urlApi_ConsolidationAccNod: domain_compensaciones + '/WSCompensacionesBatchRR-web/webresources/WSConsolidationRR/',
   urlApi_notifications: domain_notifications + '/Notification/V2.0/Rest/',
   endpoints: {
     // Settings
@@ -144,12 +145,12 @@ export const environment = {
     nodes_rules: 'NodesRulesService/run',
     business_rules: 'BusinessRulesService/run',
     billing_files: 'BillingService/run',
+    consolidate_account_nodes: 'ConsolidationAccountNodesService/run',
     // Notifications Mails
     send_mail: 'PutMessage',
     // Supervicion Process
     process_all: 'SupervisionProcessService/read/all',
   },
-
   // RR - parameterization - billing periods
   BillingPeriods: {
     url: Url_WSBilling,
@@ -160,7 +161,14 @@ export const environment = {
       delete: 'BillingPeriodsService/delete',
     },
   },
-
+  // Billing - Supervision Process
+  BillingSupervision: {
+    url: Url_WSBilling,
+    endpoints: {
+      readAll: 'SupervisionProcessService/read/all',
+      update: 'SupervisionProcessService/update'
+    },
+  },
   // RR - validation - node
   NodesValidation: {
     url: Url_WSCompensations,
@@ -173,7 +181,6 @@ export const environment = {
       update: 'MaximoService/update',
     },
   },
-
   // RR - Billing - RR Compensated Accounts
   compensatedAccounts: {
     url: Url_WSCompensationsBatchRR,
@@ -182,76 +189,3 @@ export const environment = {
     },
   },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
-
-
-
-// const domain = 'http://100.126.19.74:7669';
-// export const environment = {
-//   production: true,
-//   URL_API: domain + '/WSCompensaciones-web/webresources/WSCompensaciones/',
-//   urlApi_bulkLoad:
-//     domain + '/CompensacionesBatch-web/webresources/CompensacionesBatch/',
-//   URL_API_BATCHRR:
-//     domain +
-//     '/WSCompensacionesBatchRR-web/webresources/WSCompensacionesBatchRR/',
-//   endpoints: {
-//     // Settings
-//     settings_all: 'SettingsService/read/all',
-//     settings_create: 'SettingsService/create',
-//     settings_update: 'SettingsService/update',
-//     settings_delete: 'SettingsService/delete',
-//     // Symptom
-//     symptom_all: 'SymptomService/readAll',
-//     symptom_create: 'SymptomService/create',
-//     symptom_update: 'SymptomService/update',
-//     symptom_delete: 'SymptomService/delete',
-//     // Origin
-//     origin_all: 'OriginTypeService/read/all',
-//     origin_create: 'OriginTypeService/create',
-//     origin_update: 'OriginTypeService/update',
-//     origin_delete: 'OriginTypeService/delete',
-//     // Stratum
-//     stratum_all: 'SocialStratumService/readAll',
-//     stratum_create: 'SocialStratumService/create',
-//     stratum_update: 'SocialStratumService/update',
-//     stratum_delete: 'SocialStratumService/delete',
-//     // Causes
-//     cause_create: 'CausesService/create',
-//     cause_delete_id: 'CausesService/delete',
-//     cause_update: 'CausesService/update',
-//     cause_read_id: 'CausesService/read/id',
-//     causes_read: 'CausesService/read/all',
-//     // System status
-//     system_status_create: 'SystemStatusService/create',
-//     system_status_delete_id: 'SystemStatusService/delete',
-//     system_status_update: 'SystemStatusService/update',
-//     system_status_read_id: 'SystemStatusService/read/id',
-//     system_status_read: 'SystemStatusService/read/all',
-//     // Priorities
-//     priority_create: 'PriorityService/create',
-//     priority_delete_id: 'PriorityService/delete',
-//     priority_update: 'PriorityService/update',
-//     priority_read_id: 'PriorityService/read/id',
-//     priorities_read: 'PriorityService/readAll',
-//     // Bulk load
-//     bulk_load_create: 'sendFile',
-//     bulk_load_read: 'findTypeProcess',
-//     // Faults
-//     faults_load: 'LoadService/load',
-//     faults_read_id: 'LoadService/read/id',
-//     // Maintenance Orders Causes
-//     maintenanceOrderCause_create: 'maintenanceOrderCause/create',
-//     maintenanceOrderCause_delete: 'maintenanceOrderCause/delete',
-//     maintenanceOrderCause_update: 'maintenanceOrderCause/update',
-//     maintenanceOrdersCauses_read: 'maintenanceOrderCause/list',
-//   },
-// };
